25e5a48449fe8ef437751bb1e4e6e5024ab08c04
Jeremiah Ogutu <jogutu@pm.me>
2020-03-07 19:28:45 -0500

    updating 3 point counter

--- app/src/main/java/com/hammerhead/courtcounter/MainActivity.java
+++ app/src/main/java/com/hammerhead/courtcounter/MainActivity.java
@@ -3,12 +3,37 @@ package com.hammerhead.courtcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int scoreTeamA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayForTeamA(0);
+    }
+
+    public void threePointerForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void twoPointerForTeamA(View v) {
+        displayForTeamA(2);
+    }
+
+    public void freeThrowForTeamA(View v) {
+        displayForTeamA(1);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
     }
 }
