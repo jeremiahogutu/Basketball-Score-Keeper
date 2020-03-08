057f11cdf478693a787c3cf722eef898aec1f5ce
Jeremiah Ogutu <jogutu@pm.me>
2020-03-08 18:06:52 -0400

    Added reset button

--- app/src/main/java/com/hammerhead/courtcounter/MainActivity.java
+++ app/src/main/java/com/hammerhead/courtcounter/MainActivity.java
@@ -17,33 +17,75 @@ public class MainActivity extends AppCompatActivity {
         displayForTeamA(0);
     }
 
+    /**
+     * Increase score for team A by three points
+     * @param v
+     */
+
     public void threePointerForTeamA(View v) {
         scoreTeamA = scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
     }
+
+    /**
+     * Increase score for team B by three points
+     * @param v
+     */
 
     public void threePointerForTeamB(View v) {
         scoreTeamB = scoreTeamB + 3;
         displayForTeamB(scoreTeamB);
     }
 
+    /**
+     * Increase score for team A by two points
+     * @param v
+     */
+
     public void twoPointerForTeamA(View v) {
         scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
     }
+
+    /**
+     * Increase score for team B by 2 points
+     * @param v
+     */
 
     public void twoPointerForTeamB(View v) {
         scoreTeamB = scoreTeamB + 2;
         displayForTeamB(scoreTeamB);
     }
 
+    /**
+     * Increase score for team A by 1 point
+     * @param v
+     */
+
     public void freeThrowForTeamA(View v) {
         scoreTeamA = scoreTeamA + 1;
         displayForTeamA(scoreTeamA);
     }
 
+    /**
+     * Increase score for team B by 1 point
+     * @param v
+     */
+
     public void freeThrowForTeamB(View v) {
         scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Resets the score of both teams to 0
+     * @param v
+     */
+
+    public void resetScore(View v) {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
         displayForTeamB(scoreTeamB);
     }
 
